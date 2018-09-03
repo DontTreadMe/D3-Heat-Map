ec3bb4e87249919846fdb6f38f88deb121ab46de
DontTreadMe <31659942+DontTreadMe@users.noreply.github.com>
2018-09-03 15:17:47 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,21 +1,12 @@
 
-const margin = {
-        top: 100,
-        right: 0,
-        bottom: 100,
-        left: 120
-      },
-
+const margin = {top: 100, right: 0, bottom: 100, left: 120},
       width = 1400 - margin.right - margin.left,
-      height = 580 - margin.top - margin.bottom,
-
-      
-      formatTime = d3.timeFormat("%Y - %B"),
+      height = 680 - margin.top - margin.bottom,
       
       tip = d3.tip()
       .attr('class', 'd3-tip')
       .attr("id", "tooltip")
-      .html((d) => formatTime(new Date(d.year, d.month - 1)) + '<br>' + 
+      .html((d) => formatYM(new Date(d.year, d.month - 1)) + '<br>' + 
         d.variance.toFixed(1) + "&#8451;"),
   
       svg = d3.select("body")
@@ -23,45 +14,57 @@ const margin = {
       .attr("width", width + margin.right + margin.left)
       .attr("height", height + margin.top + margin.bottom),
       
-      div = d3.select("body")
-      .append("div")
-      .attr("id", "tooltip")
-      .style("opacity", 0),
+      formatYM = d3.timeFormat("%Y - %B"),
+      formatM = d3.timeFormat("%B"),
       
       xScale = d3.scaleLinear(),
-      yScale = d3.scaleLinear(),
+      yScale = d3.scaleBand().domain([0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11]),
       colorScale = d3.scaleLinear().range([1, 0]),
       
       xAxis = d3.axisBottom(xScale).tickFormat(d3.format("d")),
-      yAxis = d3.axisLeft(yScale);
+      yAxis = d3.axisLeft(yScale)
+      .tickFormat((m) => {
+        const date = new Date(Date.UTC(0, m));
+        return formatM(date);
+      });
 
 svg.call(tip);
 
 d3.json('https://raw.githubusercontent.com/freeCodeCamp/ProjectReferenceData/master/global-temperature.json')
   .then((json) => {
   const dataset = json.monthlyVariance;
-  const minMaxYear = d3.extent(dataset, (d) => d.year);
-  const minMaxMonth = d3.extent(dataset, (d) => d.month);
+  const minMaxYear = d3.extent(dataset, (d) => d.year);  
   const minMaxVariance = d3.extent(dataset, (d) => d.variance);
-  const quantityYear = minMaxYear[1] - minMaxYear[0] + 1;
-  const quantityMonth = minMaxMonth[1] - minMaxMonth[0] + 1;
+  const quantityYear = minMaxYear[1] - minMaxYear[0] + 1;  
   xScale.domain(minMaxYear);
   xScale.range([margin.left, width - width / quantityYear + margin.left]);
-  yScale.domain(minMaxMonth);
-  yScale.range([margin.top, height - height / quantityMonth + margin.top]);
-
-  colorScale.domain(minMaxVariance);  
-
+  yScale.range([margin.top, margin.top + height]);
+  colorScale.domain(minMaxVariance);
+  
   svg.selectAll("rect")
     .data(dataset).enter()
     .append("rect")
     .attr("x", (d) => xScale(d.year))
-    .attr("y", (d) => yScale(d.month))
+    .attr("y", (d) => yScale(d.month - 1))
     .attr("width", width / quantityYear)
-    .attr("height", height / quantityMonth)
+    .attr("height", height / 12)
     .attr("fill", (d) => d3.interpolateRdYlBu(colorScale(d.variance)))
-
     .on('mouseover', tip.show)
-    .on('mouseout', tip.hide);  
-
+    .on('mouseout', tip.hide);
+  
+  svg.append("g")
+    .attr("transform", "translate(" + 0 + "," + (height + margin.top) + ")")
+    .call(xAxis)
+    .attr("id", "x-axis");
+  
+  svg.append("g")
+  .attr("transform", "translate(" + margin.left + "," + 0 + ")")
+  .call(yAxis)
+  .attr("id", "y-axis");
+  
+  // svg.append("rect")
+  // .attr("x", margin.left)
+  // .attr("y", margin.top)
+  // .attr("width", 15)
+  // .attr("height", height)
 })
